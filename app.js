1ceaeb2af7334a7e46fa7a11f84ec5a3dd413869
angelorub94 <angelo.rubino94@gmail.com>
2015-11-05 10:18:19 +0100

    Stampa e resoconto

--- app.js
+++ app.js
@@ -9,13 +9,50 @@
  *  }
  */ 
 
-var tmpl = ' <li id="ID">' +
+/*
+              <!-- This is just an example, you should load this dynamically -->
+              <li id="1" class="current">
+                <h3>I'm a man</h3>
+              </li> 
+              <li id="2">
+                <h3>I'm a woman</h3>     
+              </li>    
+              <li id="3">
+                <h3>Hello</h3>     
+              </li>
+*/
+
+var tmpl = ' <li id="ID" class="current">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
+var correct = 0;
+var actual = 0;
+
+function cl_funct(){
+
+    $(".opt-continue").click(function(){
+            if($("input[name=iSolution]").val() === data[actual].phrase_de){
+             
+                correct++;
+                return true;
+            } 
+            actual++;
+        })        
+    return true;
+}
 
 $(document).ready(function(){
-
-  
+    for(actual=0; actual< data.length; actual++){
+    var elem = tmpl.replace("ID",actual).replace("SENTENCE",data[actual].phrase_en);
+         $(".sentences").html("");
+        $(".sentences").html(elem);
+        $("input[name=iSolution]").val("");
+        cl_funct();
+        $(".current").removeClass("current");
+    }
+    //fine ciclo
+    $("#tot-good").text(correct);
+    $("#tot").text(data.length);
 });
 
 
